431f1815250955ed714d3a0f421781ea96acad53
kushal022 <kushla.learn22@gmail.com>
2025-12-03 15:31:48 +0530

    first commit

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+  console.log("Current theme:", theme);
+  return (
+    <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+      {theme}
+    </button>
+  );
+}
